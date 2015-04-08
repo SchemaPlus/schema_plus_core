016ec40cab00b96d17d18d0c8eeffc2ae0aed751
ronen barzel <ronen@barzel.org>
2015-04-08 17:26:21 +0100

    fix parentheses warnings

--- spec/dumper_spec.rb
+++ spec/dumper_spec.rb
@@ -16,20 +16,20 @@ describe SchemaMonkey::Middleware::Dumper do
   end
 
   context TestDumper::Middleware::Dumper::Initial do
-    Then { expect(dump).to match /Schema[.]define.*do\s+#{middleware}/ }
+    Then { expect(dump).to match(/Schema[.]define.*do\s+#{middleware}/) }
   end
 
   context TestDumper::Middleware::Dumper::Tables do
-    Then { expect(dump).to match /create_table "other".*create_table "#{middleware}".*create_table "things"/m }
+    Then { expect(dump).to match(/create_table "other".*create_table "#{middleware}".*create_table "things"/m) }
   end
 
   context TestDumper::Middleware::Dumper::Table do
-    Then { expect(dump).to match /t[.]integer.*option: #{middleware} \# comment: #{middleware}/ }
-    Then { expect(dump).to match /statement: #{middleware}\s+end\s+(add_index.*)?\s+trailer: #{middleware}/ }
+    Then { expect(dump).to match(/t[.]integer.*option: #{middleware} \# comment: #{middleware}/) }
+    Then { expect(dump).to match(/statement: #{middleware}\s+end\s+(add_index.*)?\s+trailer: #{middleware}/) }
   end
 
   context TestDumper::Middleware::Dumper::Indexes do
-    Then { expect(dump).to match /add_index.*#{middleware}/ }
+    Then { expect(dump).to match(/add_index.*#{middleware}/) }
   end
 
 
