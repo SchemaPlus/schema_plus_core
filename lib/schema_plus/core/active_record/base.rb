8634370c04d4a1b0d54d70d620a1223f054e7233
ronen barzel <ronen@barzel.org>
2015-04-26 11:38:18 +0100

    Add comment for return value

--- lib/schema_plus/core/active_record/base.rb
+++ lib/schema_plus/core/active_record/base.rb
@@ -16,30 +16,30 @@ module SchemaPlus
             end
           end
 
+          # has_many, has_one, has_and_belongs_to_many, and belongs_to
+          # don't have a documented return value.  But at least one gem
+          # (https://github.com/hzamani/active_record-acts_as/blob/master/lib/active_record/acts_as/relation.rb#L14)
+          # relies on the undocumented return value.
           def has_many(name, scope = nil, options = {}, &extension)
-            SchemaMonkey::Middleware::Model::Association::Declaration.start(model: self, name: name, scope: scope, options: options, extension: extension) do
-            |env|
+            SchemaMonkey::Middleware::Model::Association::Declaration.start(model: self, name: name, scope: scope, options: options, extension: extension) do |env|
               env.result = super(env.name, env.scope, env.options, &env.extension)
             end.result
           end
 
           def has_one(name, scope = nil, options = {}, &extension)
-            SchemaMonkey::Middleware::Model::Association::Declaration.start(model: self, name: name, scope: scope, options: options, extension: extension) do
-            |env|
+            SchemaMonkey::Middleware::Model::Association::Declaration.start(model: self, name: name, scope: scope, options: options, extension: extension) do |env|
               env.result = super(env.name, env.scope, env.options, &env.extension)
             end.result
           end
 
           def has_and_belongs_to_many(name, scope = nil, options = {}, &extension)
-            SchemaMonkey::Middleware::Model::Association::Declaration.start(model: self, name: name, scope: scope, options: options, extension: extension) do
-            |env|
+            SchemaMonkey::Middleware::Model::Association::Declaration.start(model: self, name: name, scope: scope, options: options, extension: extension) do |env|
               env.result = super(env.name, env.scope, env.options, &env.extension)
             end.result
           end
 
           def belongs_to(name, scope = nil, options = {}, &extension)
-            SchemaMonkey::Middleware::Model::Association::Declaration.start(model: self, name: name, scope: scope, options: options, extension: extension) do
-            |env|
+            SchemaMonkey::Middleware::Model::Association::Declaration.start(model: self, name: name, scope: scope, options: options, extension: extension) do |env|
               env.result = super(env.name, env.scope, env.options, &env.extension)
             end.result
           end
