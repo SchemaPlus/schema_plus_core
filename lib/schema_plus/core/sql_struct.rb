1868ceb44de2215dbab879b0990fed007b0a71a3
ronen barzel <ronen@barzel.org>
2016-01-16 07:32:29 -0500

    tidy up sql parser

--- lib/schema_plus/core/sql_struct.rb
+++ lib/schema_plus/core/sql_struct.rb
@@ -4,28 +4,29 @@ module SchemaPlus
       IndexComponents = KeyStruct[:name, :type, :columns, :options, :algorithm, :using]
 
       class Table < KeyStruct[:command, :name, :body, :options, :quotechar, :inheritance]
-        INHERITANCE = 'inheritance'
-        INHERITANCE_KEY = 'INHERITS'
-        INHERITANCE_REGEX = '(?<inheritance>INHERITS \s* \( .* \)) \s*'
+
+        INHERITANCE_REGEX = %r{ \s* (?<inheritance>INHERITS \s* \( [^)]* \)) }mxi
 
         def parse!(sql)
           m = sql.strip.match %r{
-          ^
+          \A
           (?<command>.*\bTABLE\b) \s*
             (?<quote>['"`])(?<name>\S+)\k<quote> \s*
             \( \s*
             (?<body>.*) \s*
             \) \s*
-            #{INHERITANCE_REGEX if sql[INHERITANCE_KEY]}
+            # can't use optional ? for inheritance because it would be greedily grabbed into body;
+            # ideally this would use an actual parser rather than regex
+            #{INHERITANCE_REGEX if sql.match INHERITANCE_REGEX}
             (?<options> \S.*)?
-            $
+          \Z
           }mxi
           self.command = m[:command]
           self.quotechar = m[:quote]
           self.name = m[:name]
           self.body = m[:body]
           self.options = m[:options]
-          self.inheritance = m[:inheritance] if m.names.include? INHERITANCE
+          self.inheritance = m[:inheritance] rescue nil
         end
         def assemble
           ["#{command} #{quotechar}#{name}#{quotechar} (#{body})", inheritance, options].reject(&:blank?).join(" ")
