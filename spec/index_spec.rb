8d921ed3bd66dcfa22f0a18ba5d8e05c42095817
Edward Rudd <urkle@outoforder.cc>
2018-12-03 13:15:57 -0500

    fix expression indexes in rails 5.x

---
+++ spec/index_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe SchemaMonkey::Middleware::Migration::Index do
+
+  let(:migration) {::ActiveRecord::Migration}
+  let(:connection) {::ActiveRecord::Base.connection}
+
+  describe '#add_index' do
+    before(:each) do
+      migration.create_table :things, :force => true do |t|
+        t.string :test_column
+      end
+    end
+
+    context "when add an index with an expression", postgresql: :only do
+      it 'should create the index successfully' do
+        migration.add_index 'things', 'lower(test_column)'
+        expect(connection.indexes(:things).first.columns).to eq('lower((test_column)::text)')
+      end
+    end
+  end
+end
