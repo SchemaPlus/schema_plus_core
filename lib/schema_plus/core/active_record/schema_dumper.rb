3520be1fdc8e2f9c2a02ad26b9bcf8c6c628ef70
Edward Rudd <urkle@outoforder.cc>
2018-12-21 17:18:39 -0500

    fix dumping expression indexes

--- lib/schema_plus/core/active_record/schema_dumper.rb
+++ lib/schema_plus/core/active_record/schema_dumper.rb
@@ -44,6 +44,54 @@ module SchemaPlus
           end
         end
 
+        TABLE_COLUMN_MATCHES = [
+            [ # first match expression index case
+                %r{
+                  ^
+                  t\.index \s*
+                    "(?<index_cols>(?:[^"\\]|\\.)*?)" \s*
+                    , \s*
+                    name\: \s* [:'"](?<name>[^"\s]+)[,"]? \s*
+                    ,? \s*
+                    (?<options>.*)
+                  $
+                  }x,
+                ->(m) {
+                  index_cols = m[:index_cols].gsub('\\"', '"')
+                  SchemaDump::Table::Index.new name: m[:name], columns: index_cols, options: eval("{" + m[:options] + "}")
+                }
+            ],
+            [ # general matching of columns
+                %r{
+                  ^
+                  t\.(?<type>\S+) \s*
+                    [:'"](?<name>[^"\s]+)[,"]? \s*
+                    ,? \s*
+                    (?<options>.*)
+                  $
+                  }x,
+                ->(m) {
+                  SchemaDump::Table::Column.new name: m[:name], type: m[:type], options: eval("{" + m[:options] + "}"), comments: []
+                }
+            ],
+            [ # index definitions with multiple columns
+                %r{
+                  ^
+                  t\.index \s*
+                    \[(?<index_cols>.*?)\] \s*
+                    , \s*
+                    name\: \s* [:'"](?<name>[^"\s]+)[,"]? \s*
+                    ,? \s*
+                    (?<options>.*)
+                  $
+                  }x,
+                ->(m) {
+                  index_cols = m[:index_cols].tr(%q{'":}, '').strip.split(/\s*,\s*/)
+                  SchemaDump::Table::Index.new name: m[:name], columns: index_cols, options: eval("{#{m[:options]}}")
+                }
+            ]
+        ].freeze
+
         def table(table, _)
           SchemaMonkey::Middleware::Dumper::Table.start(dumper: self, connection: @connection, dump: @dump, table: @dump.tables[table] = SchemaDump::Table.new(name: table)) do |env|
             stream = StringIO.new
@@ -68,34 +116,13 @@ module SchemaPlus
               env.table.trailer = m[:trailer].split("\n").map(&:strip).reject{|s| s.blank?}
               table_objects = m[:columns].strip.split("\n").map { |col|
                 cs = col.strip
-                m = cs.match %r{
-                ^
-                t\.(?<type>\S+) \s*
-                  [:'"](?<name>[^"\s]+)[,"]? \s*
-                  ,? \s*
-                  (?<options>.*)
-                $
-                }x
-                if !m.nil?
-                  SchemaDump::Table::Column.new name: m[:name], type: m[:type], options: eval("{" + m[:options] + "}"), comments: []
-                else
-                  m = cs.match %r{
-                  ^
-                  t\.index \s*
-                    \[(?<index_cols>.*?)\] \s*
-                    , \s*
-                    name\: \s* [:'"](?<name>[^"\s]+)[,"]? \s*
-                    ,? \s*
-                    (?<options>.*)
-                  $
-                  }x
-                  if m.nil?
-                    nil
-                  else
-                    index_cols = m[:index_cols].tr(%q{'":}, '').strip.split(/\s*,\s*/)
-                    SchemaDump::Table::Index.new name: m[:name], columns: index_cols, options: eval("{#{m[:options]}}")
-                  end
+                result = nil
+                # find the first regex that matches and grab the column definition
+                TABLE_COLUMN_MATCHES.find do |(r, l)|
+                  m = cs.match r
+                  result = m.nil? ? nil : l.call(m)
                 end
+                result
               }.reject { |o| o.nil? }
               env.table.columns = table_objects.select { |o| o.is_a? SchemaDump::Table::Column }
               env.table.indexes = table_objects.select { |o| o.is_a? SchemaDump::Table::Index }
